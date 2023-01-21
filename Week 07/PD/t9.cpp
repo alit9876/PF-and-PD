435a0f455c85e2e1a4b3db67383847d3c90159ae
DELL <m.alit9876@gmail.com>
2023-01-21 14:12:24 +0500

    Week-07

--- Week 07/PD/t9.cpp
+++ Week 07/PD/t9.cpp
@@ -5,21 +5,22 @@ main()
     int row;
     cout << "Enter desired number: ";
     cin >> row;
-    for (int n = 1; n <= row; n++)
+    for (int i = 1; i <= row; i++)
     {
-        for (int j = 1; j <= n; j++)
+        for (int j = 1; j <= i; j++)
         {
             cout << "*";
-            for(int i = row; i >= row-2; row--)
-            {
-                cout << " ";
-                cout << "*";
-            }
-            
-            
+        }
+
+        for (int s = i + 1; (s <= row * 2 - i); s++)
+        {
+            cout << " ";
+        }
+
+        for (int t = (row - i); t < row; t++)
+        {
+            cout << "*";
         }
         cout << endl;
     }
-
-   
 }
--- Week 07/PF/t6.cpp
+++ Week 07/PF/t6.cpp
@@ -14,21 +14,25 @@ main()
     hcf = ecd(number1, number2);
     leastCF = lcm(number1, number2, hcf);
     cout << "HCF: " << hcf << endl;
-    cout << "leastCf: " << lcm << endl;
+    cout << "leastCf: " << leastCF << endl;
 }
 
 int ecd(int number1, int number2)
 {
-    int n;
-    for (n = 2;( n <= number1 && (n <= number2)) && (number1 % n == 0) && (number2 % n == 0); n++)
+    int hcf;
+    for (int n = 2; (n <= number1 && (n <= number2)); n++)
     {
-      return n;
+        if ((number1 % n == 0) && (number2 % n == 0))
+        {
+            hcf = n;
+        }
     }
+    return hcf;
 }
 
 int lcm(int number1, int number2, int hcf)
 {
     int lcm;
-    lcm = (number1 * number2) / hcf;
+    lcm = (number1 * number2) / ecd(number1,number2);
     return lcm;
 }
